1f69bdbc75ff715adb14438ec9454f9d2fb67653
zenitba <112856019+zenitba@users.noreply.github.com>
2024-01-12 10:06:53 +0100

    image verkleint

--- src/lib/molecules/Grot.svelte
+++ src/lib/molecules/Grot.svelte
@@ -1,7 +1,7 @@
 <script>
-  import { onMount } from "svelte"
+  import { onMount } from "svelte";
 
-  onMount(() => {})
+  onMount(() => {});
 </script>
 
 <!--==================== FOOTER ILLUSTRATIONS ====================-->
@@ -9,20 +9,27 @@
   <img
     class="cave-ground"
     src="/assets/footergrot1.png"
-    alt=""
+    alt="Grot Illustratie"
     loading="lazy"
+    height="350px"
+    width="100%"
+    aria-label="Cave Ground Illustration"
   />
   <img
     class="water-drup"
     src="/assets/drup.png"
-    alt=""
+    alt="Grot Illustratie"
     loading="lazy"
+    aria-label="Water Droplet Illustration"
   />
   <img
     class="cave-water"
     src="/assets/footergrot2.png"
-    alt=""
+    alt="Grot Illustratie"
     loading="lazy"
+    height="350px"
+    width="100%"
+    aria-label="Cave Water Illustration"
   />
 </section>
 
@@ -34,47 +41,40 @@
   }
 
   img {
-    width: 100%;
     margin: 0;
     padding: 0;
+    width: 100%;
+
   }
 
   .cave-ground {
     position: relative;
     z-index: 10;
     display: block;
+    width: 100%;
   }
 
   .cave-water {
     position: relative;
     z-index: -1;
     margin-top: -200%;
+    width: 100%; 
   }
 
   .water-drup {
     width: 1rem;
     z-index: 100;
-    transform: translateY(
-      -50vw
-    );
+    transform: translateY(-50vw);
     margin-left: 30%;
-    /* border: 3px solid red; */
-    animation: fall
-      2s ease-in
-      infinite;
+    animation: fall 2s ease-in infinite;
   }
 
   @keyframes fall {
     0% {
-      transform: translateY(
-        -50vw
-      );
+      transform: translateY(-30vw);
     }
     100% {
-      transform: translateY(
-        0vw
-      );
+      transform: translateY(0vw);
     }
   }
-  /* hoi test test */
 </style>
