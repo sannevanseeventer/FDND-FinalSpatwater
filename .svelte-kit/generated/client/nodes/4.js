aea73db5430d8d9a690f362752be85dd4f5436e0
ThomasScheepers <thomas@scheepers.com>
2024-01-16 09:38:46 +0100

    Added importand svelte files

---
+++ .svelte-kit/generated/client/nodes/4.js
@@ -0,0 +1,1 @@
+export { default as component } from "../../../../src/routes/kennisbank/+page.svelte";
---
+++ .svelte-kit/generated/client/nodes/6.js
@@ -0,0 +1,1 @@
+export { default as component } from "../../../../src/routes/projecten/+page.svelte";
