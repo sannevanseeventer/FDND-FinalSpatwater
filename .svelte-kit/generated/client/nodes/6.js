3c0a7cb2e2d65542fab77571d049c9a0944c2386
Tjips <thomas@scheepers.com>
2024-01-25 14:34:35 +0100

    Alles weg

--- .svelte-kit/generated/client/nodes/6.js
+++
@@ -1,1 +0,0 @@
-export { default as component } from "../../../../src/routes/projecten/+page.svelte";
--- src/routes/+layout.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-    import Navbar from "$lib/organisms/Navbar.svelte"
-    import NavbarV2 from "$lib/organisms/NavbarV2.svelte"
-</script>
-
-<!-- <Navbar /> -->
-<NavbarV2 />
-<slot />
